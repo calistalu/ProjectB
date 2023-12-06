c14450cbd29d91a85bc7d284f87f086f279b2ce5
calistalu <143591016+calistalu@users.noreply.github.com>
2023-12-07 00:51:49 +0800

    finally！！！

--- js/basic.js
+++ js/basic.js
@@ -18,3 +18,35 @@ window.addEventListener("scroll", function() {
 function redirect() {
   window.location.href = 'index2.html';
 }
+
+var audio = new Audio('audio/light.wav');
+audio.loop = true; 
+var audio2= new Audio('audio/pop.wav')
+
+function playMusic() {
+  audio.play();
+}
+
+function pauseMusic() {
+  audio.pause();
+}
+
+document.getElementById('musicBtn').addEventListener('click', function() {
+  if (audio.paused) {
+    playMusic();
+    document.getElementById('musicBtn').textContent = 'Pause Music';
+  } else {
+    pauseMusic();
+    document.getElementById('musicBtn').textContent = 'Listen';
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var frames = [];
+  for (let i = 1; i <= 4; i++) {
+    frames.push(document.querySelector('#f' + i));
+    frames[i - 1].addEventListener('mouseenter', function() {
+      audio2.play();
+    });
+  }
+});
--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,5 @@
 var song;
+var star;
 var img;
 var backgroundImage;
 var totalnum = 100;
@@ -9,38 +10,63 @@ var imgIndex = 1;
 var Mode = 0;
 var drops = [];
 var hasPlayed = false;
+
 var child;
 var teen;
 var adult;
 var old;
+let w1;
+let w2;
+let w3;
+let w4;
+let end;
+
 let amplitude;
 let snowflakes = [];
+let start = false;
 
+var increasing=false
+var previousAmp; 
+let randomIndex1
+let randomIndex2
+let randomIndex3
 let currentImage = "child";
-let displayText = "Press right arrow to move on";
+let displayText = "Click to start. Press right arrow to move on";
+let count1=0
+let count2=0
+let count3=0
+let count4=0
 
 function preload() {
     song = loadSound("audio/music.mp3");
-
+    star = loadImage("image/star.jpg")
     img = loadImage('image/background.jpg');
     child = loadImage('image/child-unscreen.gif')
     teen = loadImage('image/teen-unscreen.gif')
     adult = loadImage('image/adult-unscreen.gif')
     old = loadImage('image/old-unscreen.gif')
+    w1 = loadImage('image/w1.gif')
+    w2 = loadImage('image/w2.gif')
+    w3 = loadImage('image/w3.gif')
+    w4 = loadImage('image/w4.gif')
+    end = loadImage('image/end.jpg')
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     
     fft = new p5.FFT();
+    amplitude = new p5.Amplitude();
 
     let button = createButton('RAIN');
-    button.position(width - 70, height - 30);
+    button.position(width - 130, height - 80);
     button.mousePressed(changeMode);
+    button.size(100,50)
 
     let button2 = createButton('SNOW');
-    button2.position(width - 170, height - 30);
+    button2.position(width - 230, height - 80);
     button2.mousePressed(changeMode2);
+    button2.size(100,50)
 
     amplitude = new p5.Amplitude();
 }
@@ -50,33 +76,141 @@ function draw() {
     imageMode(CORNER);
     image(img, 0, 0, width, height);
 
-   
-
     background(0, 100)
 
     
     imageMode(CENTER);
     if (currentImage === "child") {
+        if (start){
+        count1 ++;
+        if (count1<150){
+            image(w1, map(count1,0,150,0,width/4), height-100, 90,120)
+            stroke(255,150)
+            strokeWeight(10)
+            line(0,height-90,map(count1,0,150,0,width/4), height-90)
+        }else{
+            count1=201
+            stroke(255,150)
+            strokeWeight(10)
+            image(w1, width/4, height-100, 90,120)
+            line(0,height-90,width/4, height-90)
+        }
         image(child, width/2, height/2-100-150, 150, 200);
+    }
         textAlign(CENTER, CENTER);
         textSize(20);
         fill(255);
+        strokeWeight(1)
         text(displayText, width/2, height - 50);
     } else if (currentImage === "teen") {
+        count2 ++;
+        if (count2<150){
+            image(w2, map(count2,0,150,width/4,width/2), height-100, 90,120)
+            stroke(255,150)
+            strokeWeight(10)
+            line(0,height-90,map(count2,0,150,width/4,width/2), height-90)
+        }else{
+            count2=201
+            stroke(255,150)
+            strokeWeight(10)
+            image(w2, width/2, height-100, 90,120)
+            line(0,height-90,width/2, height-90)
+        }
         image(teen, width/2, height/2-100-150, 150, 200);
     } else if (currentImage === "adult") {
+        count3 ++;
+        if (count3<150){
+            image(w3, map(count3,0,150,width/2,3*width/4), height-100, 90,120)
+            stroke(255,150)
+            strokeWeight(10)
+            line(0,height-90,map(count3,0,150,width/2,3*width/4), height-90)
+        }else{
+            count3=201
+            stroke(255,150)
+            strokeWeight(10)
+            image(w3, 3*width/4, height-100, 90,120)
+            line(0,height-90,3*width/4, height-90) 
+        }
         image(adult, width/2, height/2-100-150, 150, 200);
     } else if (currentImage === "old") {
+        count4 ++;
+        if (count4<150){
+            image(w4, map(count4,0,150,3*width/4,width), height-100, 90,120)
+            stroke(255,150)
+            strokeWeight(10)
+            line(0,height-90,map(count4,0,150,3*width/4,width), height-90)
+        }else{
+            count4=201
+            stroke(255,150)
+            strokeWeight(10)
+            image(w4, width, height-100, 90,120)
+            line(0,height-90,width, height-90) 
+        }
         image(old, width/2, height/2-100-150, 150, 200);
+    }else if (currentImage === "end") {
+        push()
+        translate(width/2,height/2)
+        image(end,0,0,width,height)
+        pop()
     }
 
-
+   
     fft.analyze();
     var amp =  fft.getEnergy(20, 200);
+
+    
+  let Amp = amplitude.getLevel();
+
+    if (Amp - previousAmp > 0.05) {
+        increasing=true
+        
+    }else{
+        increasing=false
+    }
+    previousAmp = Amp;
+
+
+    if(increasing){ 
+           randomIndex1 = floor(random(particles.length));
+           randomIndex2 = floor(random(particles.length));
+           randomIndex3 = floor(random(particles.length));
+        }
+            
+        if (particles[randomIndex1]) {
+            push()
+            translate(width/2, height/2+100)
+            translate(particles[randomIndex1].x, particles[randomIndex1].y)
+            rotate(frameCount)
+            image(star, 0,0, 30, 30);
+            pop()
+        }
+            
+        if (particles[randomIndex2]) {
+            push()
+            translate(width/2, height/2+100)
+            translate(particles[randomIndex2].x, particles[randomIndex2].y)
+            rotate(frameCount)
+            image(star, 0,0, 40, 40);
+            pop()
+        }
+            
+        if (particles[randomIndex3]) {
+            push()
+            translate(width/2, height/2+100)
+            translate(particles[randomIndex3].x, particles[randomIndex3].y)
+            rotate(frameCount)
+            image(star, 0,0, 50, 50);
+            pop()
+        }
+
+
+
 
     stroke(amp / 1.5, 100, 150);
     strokeWeight(3);
     noFill();
+
+
 
     var wave = fft.waveform();
 
@@ -100,25 +234,27 @@ function draw() {
     }
     pop()
 
+    if (start==true){
     push()
-    translate(0, 100)
-    translate(width / 2, height / 2)
+    translate(width / 2, height / 2+100)
     rotate(-ang)
     ang += amp / 250
+    
 
     var p = new Particle();
     if (frameCount % 20 == 0) {
         particles.push(p);
     }
-
-
     for (var i = particles.length - 1; i >= 0; i--) {
-
+        
         particles[i].update();
         particles[i].show();
-
     }
     pop()
+    }
+  
+   
+
     if (Mode == 1) {
 
         drops.push(new Drop(random(windowWidth), 0, 0))
@@ -145,6 +281,9 @@ function draw() {
 
 }
 function mousePressed() {
+    start=true
+    
+  
     if (!hasPlayed) {
         song.play();
         hasPlayed = true;
@@ -159,6 +298,8 @@ function keyPressed() {
             currentImage = "adult";
         } else if (currentImage === "adult") {
             currentImage = "old";
+        }else if (currentImage === "old") {
+            currentImage = "end";
         }
     }
 }
@@ -199,9 +340,15 @@ class Particle {
         }
     }
 
-    turn(){
-
+    showImage(img) {
+        this.count++
+        push();
+        translate(width/2,height/2+100);
+        rotate(frameCount);
+        image(img, this.x, this.y,50,50);
+        pop();
     }
+
 }
 
 class Drop {
